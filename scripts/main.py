86be7749100e79428c3437f028f1675fb746ebd0
Matt Holden <matthew.holden86@gmail.com>
2024-10-30 22:45:54 -0400

    begin main pipeline

--- scripts/main.py
+++ scripts/main.py
@@ -1,2 +1,11 @@
-from utils import plotting
-print("Done")
+from data_processing.court_masking.court_isolation import *
+from data_processing.frame_extraction.frame_extraction import extract_frames
+from data_processing.homography.homography import HomographyHandler, save_matrix, frame_cropper
+from utils.plotting import plt_plot
+from utils.resize_frame import resize_img
+from google_drive.data_upload import upload_file_to_drive
+
+def processing_pipeline(video_id):
+    pass
+
+
