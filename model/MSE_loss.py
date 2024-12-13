6d515f947096fc7011bd1e187fa367657cd406db
Matt Holden <matthew.holden86@gmail.com>
2024-12-13 11:32:13 -0500

    MSE loss test class complete

--- model/MSE_loss.py
+++ model/MSE_loss.py
@@ -6,5 +6,11 @@ class MSELoss(nn.Module):
     def __init__(self):
         super(MSELoss, self).__init__()
 
-    def forward(self, pred, target):
-        return torch.nn.functional.mse_loss(pred, target)
+    def forward(self, H_pred, H_gt):
+
+        if H_pred.ndim != 3 or H_pred.shape[1:] != (3, 3):
+            raise ValueError("H_pred must have shape [batch_size, 3, 3].")
+        if H_gt.ndim != 3 or H_gt.shape[1:] != (3, 3):
+            raise ValueError("H_gt must have shape [batch_size, 3, 3].")
+
+        return torch.nn.functional.mse_loss(H_pred, H_gt)
